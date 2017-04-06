e441ac80b0fa15a56d0f5607b91b0b5d1b6a0eec
wuwenjie03 <wuwenjie03@meituan.com>
2017-04-06 12:14:18 +0800

    add lr, add performance test in lr && ftrl

--- ftrl/lr.py
+++ ftrl/lr.py
@@ -35,9 +35,6 @@ print "train -> {0}, test -> {1}, submission -> {2}".format(train, test, submiss
 
 # B, model
 alpha = .1  # learning rate
-beta = 1.   # smoothing parameter for adaptive learning rate
-L1 = 1.     # L1 regularization, larger value means more regularized
-L2 = 1.     # L2 regularization, larger value means more regularized
 
 # C, feature/hash trick
 # D = 2 ** 20              # number of weights to use
@@ -99,128 +96,6 @@ class logistic_regression(object):
         # update w
         for i in x:
             w[i] += g * alpha
-
-
-class ftrl_proximal(object):
-    ''' Our main algorithm: Follow the regularized leader - proximal
-
-        In short,
-        this is an adaptive-learning-rate sparse logistic-regression with
-        efficient L1-L2-regularization
-
-        Reference:
-        http://www.eecs.tufts.edu/~dsculley/papers/ad-click-prediction.pdf
-    '''
-
-    def __init__(self, alpha, beta, L1, L2, D, interaction=False):
-        # parameters
-        self.alpha = alpha
-        self.beta = beta
-        self.L1 = L1
-        self.L2 = L2
-
-        # feature related parameters
-        self.D = D
-        self.interaction = interaction
-        self.interaction_dict = {}
-
-        # model
-        # n: squared sum of past gradients
-        # z: weights
-        # w: lazy weights
-        self.n = [0.] * D
-        self.z = [0.] * D
-        self.w = [0.] * D  # use this for execution speed up
-        # self.w = {}  # use this for memory usage reduction
-
-    def _indices(self, x):
-        ''' A helper generator that yields the indices in x
-
-            The purpose of this generator is to make the following
-            code a bit cleaner when doing feature interaction.
-        '''
-
-        for i in x:
-            yield i
-
-        if self.interaction:
-            D = self.D
-            L = len(x)
-            for i in xrange(1, L):  # skip bias term, so we start at 1
-                for j in xrange(i+1, L):
-                    yield (i * j) % D
-
-    def predict(self, x):
-        ''' Get probability estimation on x
-
-            INPUT:
-                x: features
-
-            OUTPUT:
-                probability of p(y = 1 | x; w)
-        '''
-
-        # parameters
-        alpha = self.alpha
-        beta = self.beta
-        L1 = self.L1
-        L2 = self.L2
-
-        # model
-        n = self.n
-        z = self.z
-        w = self.w  # use this for execution speed up
-        # w = {}  # use this for memory usage reduction
-
-        # wTx is the inner product of w and x
-        wTx = 0.
-        for i in self._indices(x):
-            sign = -1. if z[i] < 0 else 1.  # get sign of z[i]
-
-            # build w on the fly using z and n, hence the name - lazy weights -
-            if sign * z[i] <= L1:
-                # w[i] vanishes due to L1 regularization
-                w[i] = 0.
-            else:
-                # apply prediction time L1, L2 regularization to z and get w
-                w[i] = (sign * L1 - z[i]) / ((beta + sqrt(n[i])) / alpha + L2)
-
-            wTx += w[i]
-
-        self.w = w
-
-        # bounded sigmoid function, this is the probability estimation
-        return 1. / (1. + exp(-max(min(wTx, 35.), -35.)))
-
-    def update(self, x, p, y):
-        ''' Update model using x, p, y
-
-            INPUT:
-                x: feature, a list of indices
-                p: click probability prediction of our model
-                y: answer
-
-            MODIFIES:
-                self.n: increase by squared gradient
-                self.z: weights
-        '''
-
-        # parameter
-        alpha = self.alpha
-
-        # model
-        n = self.n
-        z = self.z
-        w = self.w  # no need to change this, it won't gain anything
-
-        # gradient under logloss
-        g = p - y
-
-        # update z and n
-        for i in self._indices(x):
-            sigma = (sqrt(n[i] + g * g) - sqrt(n[i])) / alpha
-            z[i] += g - sigma * w[i]
-            n[i] += g * g
 
 
 def logloss(p, y):
@@ -287,7 +162,7 @@ def data(path, D):
 start = datetime.now()
 
 # initialize ourselves a learner
-learner = ftrl_proximal(alpha, beta, L1, L2, D, interaction=do_interactions)
+learner = logistic_regression(alpha, D)
 
 # start training
 for e in xrange(epoch):
@@ -326,15 +201,11 @@ for e in xrange(epoch):
 # start testing, and build Kaggle's submission file ##########################
 ##############################################################################
 
-predict_time = 0
 with open(submission, 'w') as outfile:
     for t, ID, x, y in data(test, D):
         start_time = time.time()
         p = learner.predict(x)
         end_time = time.time()
-        interval = end_time - start_time
-        predict_time += interval
+        predict_time_used = end_time - start_time
+        print("predict time used --> " + predict_time_used)
         outfile.write('%s,%s\n' % (ID, str(p)))
-
-print("total non-zero weights --> " + str(len(filter(lambda x: x != 0, learner.w))))
-print("predict time used --> " + predict_time)
