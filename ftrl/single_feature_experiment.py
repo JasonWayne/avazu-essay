e37763eb8bac50d7803775ed5655fa582e84de1f
jasonwayne <wuwenjie0102@gmail.com>
2017-04-06 12:00:58 +0800

    fix bug in to_libsvm && add old files

--- ftrl/single_feature_experiment.py
+++ ftrl/single_feature_experiment.py
@@ -1,5 +1,5 @@
 import subprocess
 
-for i in range(3, 23):
+for i in range(23, 24):
     print "\n\n\nrun field " + str(i) + "\n"
-    subprocess.call("python ftrl/ftrl.py train.raw.csv test.raw.csv submission.csv {0}".format(i).split(" "), shell=True)
+    subprocess.call("python ftrl/ftrl.py train.raw.csv test.raw.csv submission.csv {0}".format(i).split(" "), shell=False)
--- gbdt/to_libsvm.py
+++ gbdt/to_libsvm.py
@@ -3,10 +3,12 @@ import sys
 in_path = sys.argv[1]
 out_path = sys.argv[2]
 
+filtered_ids = set([999985, 999984, 999990, 999986, 999981, 999980, 999983, 999982, 999976, 999977, 999989, 999987, 999988, 999978, 999979])
+
 with open(in_path) as f_in:
     with open(out_path, 'w') as f_out:
         for line in f_in:
             arr = line.strip().split(" ")
             label = int(arr[1])
-            feature_ids = arr[2:]
-            f_out.write(str(label) + " " + " ".join(map(lambda s: s + ":1", feature_ids)) + "\n")
+            feature_ids = sorted(filter(lambda x: x not in filtered_ids, map(lambda x: int(x), arr[2:])))
+            f_out.write(str(label) + " " + " ".join(map(lambda s: str(s) + ":1", feature_ids)) + "\n")
