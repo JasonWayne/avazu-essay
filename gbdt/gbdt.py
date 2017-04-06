031bc46321870399c1dd69b7edb1c97e96a49d89
wuwenjie03 <wuwenjie03@meituan.com>
2017-04-06 11:21:14 +0800

    add xgboost train & predict

--- gbdt/gbdt.py
+++ gbdt/gbdt.py
@@ -1,1 +1,22 @@
 import xgboost as xgb
+import sys
+import time
+
+train_file = sys.argv[1]
+test_file = sys.argv[2]
+
+# read in data
+dtrain = xgb.DMatrix(train_file)
+dtest = xgb.DMatrix(test_file)
+
+# specify parameters via map
+param = {'max_depth': 5, 'eta': 0.3, 'silent': 1, 'objective':'binary:logistic' }
+num_round = 50
+bst = xgb.train(param, dtrain, num_round)
+# make prediction
+start_time = time.time()
+preds = bst.predict(dtest)
+end_time = time.time()
+predict_time = end_time - start_time
+print("predict time is " + str(predict_time) + " seconds!!!!")
+
